50aaed12a8b690a6c0eae1be757b905cbc2271b5
zhaomanli <73468060+zhaomanli@users.noreply.github.com>
2022-12-08 19:21:35 +0800

    Create ext11.py

---
+++ ext11.py
@@ -0,0 +1,8 @@
+print('How old are you?',end=' ')
+age = input() + 'years'
+print('How tall are you?',end=' ')
+height = input() + 'cm'
+print('How much do you weigh?',end=' ')
+weight = input() + 'kg'
+
+print(f'So,you\'re {age} old,{height} tall and {weight} heavy.')

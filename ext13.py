270b8ce454667d35170ec5ba4f2a823d943aa97b
zhaomanli <73468060+zhaomanli@users.noreply.github.com>
2022-12-08 19:23:19 +0800

    Create ext13.py

---
+++ ext13.py
@@ -0,0 +1,10 @@
+from sys import argv
+#read the WYSS esction for how to run this
+script,first,second,third = argv
+first = input('Enter a variable: ')
+second = input('Enter a variable: ')
+third = input('Enter a variable: ')
+print('The script is called: ',script)
+print('Your first varibable is: ',first)
+print('Your second variable is: ',second)
+print('Your third variable is: ',third)

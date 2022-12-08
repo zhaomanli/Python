09013b522ccfe9beb8e0be80e30db46c7259d917
zhaomanli <73468060+zhaomanli@users.noreply.github.com>
2022-12-08 19:22:52 +0800

    Create ext12.py

---
+++ ext12.py
@@ -0,0 +1,6 @@
+# age = input('How old are you? ')
+# height = input('How tall are you? ')
+# weight = input('How much do you weight? ')
+
+# print(f'So,you\'re {age} years old,{height}cm tall,{weight}kg heavy.')
+print('How old are you?',input())
